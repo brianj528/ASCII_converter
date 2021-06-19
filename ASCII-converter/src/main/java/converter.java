7f4c66b9ca9ce7ad4986a1a9bff432a58886adb2
brianj528 <brianj528@gmail.com>
2021-06-19 13:01:08 -0400

    Update converter.java
    cleaned up some extra code

--- ASCII-converter/src/main/java/converter.java
+++ ASCII-converter/src/main/java/converter.java
@@ -1,11 +1,9 @@
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class converter {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         System.out.println("Please enter the string to be converted: ");
         Scanner userInput = new Scanner(System.in);
         String text = userInput.next();
@@ -14,7 +12,4 @@ public class converter {
         System.out.println(Arrays.toString(bytes));
 
     }
-
-
-
 }
